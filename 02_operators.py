c2f530c8d7039c058815d05222f81ff20f6cd209
AlemolAMG <alemolamg@outlook.com>
2022-08-22 12:49:12 +0200

    Apuntes en los operators.

--- 02_operators.py
+++ 02_operators.py
@@ -26,7 +26,7 @@ print("Hola " * int(my_float))
 ### Operadores Comparativos ###
 
 # Operaciones con enteros
-print(3 > 4) 
+print(3 > 4)
 print(3 < 4)
 print(3 >= 4)
 print(4 <= 4)
@@ -34,10 +34,10 @@ print(3 == 4)
 print(3 != 4)
 
 # Operaciones con cadenas de texto
-print("Hola" > "Python") 
+print("Hola" > "Python")
 print("Hola" < "Python")
-print("aaaa" >= "abaa") # Ordenación alfabética por ASCII
-print(len("aaaa") >= len("abaa")) # Cuenta caracteres
+print("aaaa" >= "abaa")  # Ordenación alfabética por ASCII
+print(len("aaaa") >= len("abaa"))  # Cuenta caracteres
 print("Hola" <= "Python")
 print("Hola" == "Hola")
 print("Hola" != "Python")
